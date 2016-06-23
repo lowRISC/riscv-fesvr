b0541376453d0abf99ef375d25493212f9b4429b
Wei Song <wsong83@gmail.com>
2016-06-23 11:29:46 +0100

    make elf2hex to accept a base address

--- fesvr/elf2hex.cc
+++ fesvr/elf2hex.cc
@@ -7,29 +7,41 @@
 
 int main(int argc, char** argv)
 {
-  if(argc != 4)
+  if(argc < 4 || argc > 5)
   {
-    std::cerr << "Usage: " << argv[0] << " <width> <depth> <elf_file>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [base] width depth elf_file" << std::endl;
     return 1;
   }
 
-  unsigned width = atoi(argv[1]);
-  if(width == 0 || (width & (width-1)))
+  unsigned int argc_offset = argc - 4;
+
+  unsigned int base = 0;
+  if(argc_offset) {
+    base = atoi(argv[1]);
+    if((base & (base-1)))
+    {
+      std::cerr << "base must be a power of 2" << std::endl;
+      return 1;
+    }
+  }
+
+  unsigned width = atoi(argv[1 + argc_offset]);
+  if(width < 4 || (width & (width-1)))
   {
-    std::cerr << "width must be a power of 2" << std::endl;
+    std::cerr << "width must be at least 4 and a power of 2" << std::endl;
     return 1;
   }
 
-  unsigned depth = atoi(argv[2]);
+  unsigned depth = atoi(argv[2 + argc_offset]);
   if(depth == 0 || (depth & (depth-1)))
   {
     std::cerr << "depth must be nonzero and a power of 2" << std::endl;
     return 1;
   }
 
-  htif_hexwriter_t htif(width, depth);
+  htif_hexwriter_t htif(base, width, depth);
   memif_t memif(&htif);
-  load_elf(argv[3], &memif);
+  load_elf(argv[3 + argc_offset], &memif);
   std::cout << htif;
 
   return 0;
